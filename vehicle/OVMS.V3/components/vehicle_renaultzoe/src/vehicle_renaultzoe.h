3e8f9ceda325ace8451f00cdf05819ca3a04d46a
Marcos Mezo <mmezo@selexco.net>
2018-11-09 23:57:00 +0100

    Renault Zoe - change indentig style of header file

--- vehicle/OVMS.V3/components/vehicle_renaultzoe/src/vehicle_renaultzoe.h
+++ vehicle/OVMS.V3/components/vehicle_renaultzoe/src/vehicle_renaultzoe.h
@@ -42,112 +42,112 @@ class OvmsVehicleRenaultZoe : public OvmsVehicle
     ~OvmsVehicleRenaultZoe();
   
 
-public:
-void IncomingFrameCan1(CAN_frame_t* p_frame);
-// void Ticker1(uint32_t ticker, OvmsWriter* writer);
-void Ticker1(uint32_t ticker);
-void IncomingPollReply(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain);
-void ConfigChanged(OvmsConfigParam* param);
+	public:
+	void IncomingFrameCan1(CAN_frame_t* p_frame);
+	// void Ticker1(uint32_t ticker, OvmsWriter* writer);
+	void Ticker1(uint32_t ticker);
+	void IncomingPollReply(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain);
+	void ConfigChanged(OvmsConfigParam* param);
 
 
 
-protected:
-void vehicle_renaultzoe_car_on(bool isOn);
-void UpdateMaxRangeAndSOH(void);
-uint16_t calcMinutesRemaining(float target);
-bool SendCanMessage_sync(uint16_t id, uint8_t count,
-                         uint8_t serviceId, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4,
-                         uint8_t b5, uint8_t b6);
-bool SendCommandInSessionMode(uint16_t id, uint8_t count,
-                              uint8_t serviceId, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4,
-                              uint8_t b5, uint8_t b6 );
-bool SetTemporarySessionMode(uint16_t id, uint8_t mode);
-bool SetDoorLock(bool open, const char* password);
-bool OpenTrunk(const char* password);
-bool IsPasswordOk(const char *password);
-void SetChargeMetrics(float voltage, float current, float climit, bool chademo);
+	protected:
+	void vehicle_renaultzoe_car_on(bool isOn);
+	void UpdateMaxRangeAndSOH(void);
+	uint16_t calcMinutesRemaining(float target);
+	bool SendCanMessage_sync(uint16_t id, uint8_t count,
+													 uint8_t serviceId, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4,
+													 uint8_t b5, uint8_t b6);
+	bool SendCommandInSessionMode(uint16_t id, uint8_t count,
+																uint8_t serviceId, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4,
+																uint8_t b5, uint8_t b6 );
+	bool SetTemporarySessionMode(uint16_t id, uint8_t mode);
+	bool SetDoorLock(bool open, const char* password);
+	bool OpenTrunk(const char* password);
+	bool IsPasswordOk(const char *password);
+	void SetChargeMetrics(float voltage, float current, float climit, bool chademo);
 
-// Renault ZOE specific metrics
-OvmsMetricFloat*  m_b_cell_volt_max;           // Battery cell maximum voltage
-OvmsMetricFloat*  m_b_cell_volt_min;           // Battery cell minimum voltage
-OvmsMetricFloat*  m_b_cell_det_max;               // Battery cell maximum detoriation
-OvmsMetricFloat*  m_b_cell_det_min;               // Battery cell minimum detoriation
-OvmsMetricFloat*  m_b_bat_avail_energy;               // Battery available kWh
-OvmsMetricFloat*  m_b_bat_max_charge_power;    // Max Bat Charge
-OvmsMetricFloat*  m_mains_power;                // Mains Power Consumed
-OvmsMetricFloat*  m_v_hydraulic_brake_power; // Hydraulic Brake Power Request (Losses)
-OvmsMetricFloat* m_bat_heat_sink_temp;
-OvmsMetricInt* m_charge_pilot_current;
-OvmsMetricInt* m_b_temp1;
-OvmsMetricInt* m_b_temp2;
-OvmsMetricInt* m_b_temp3;
-OvmsMetricInt* m_b_temp4;
-OvmsMetricInt* m_b_temp5;
-OvmsMetricInt* m_b_temp6;
-OvmsMetricInt* m_b_temp7;
-OvmsMetricInt* m_b_temp8;
-OvmsMetricInt* m_b_temp9;
-OvmsMetricInt* m_b_temp10;
-OvmsMetricInt* m_b_temp11;
-OvmsMetricInt* m_b_temp12;
+	// Renault ZOE specific metrics
+	OvmsMetricFloat*  m_b_cell_volt_max;         // Battery cell maximum voltage
+	OvmsMetricFloat*  m_b_cell_volt_min;         // Battery cell minimum voltage
+	OvmsMetricFloat*  m_b_cell_det_max;          // Battery cell maximum detoriation
+	OvmsMetricFloat*  m_b_cell_det_min;          // Battery cell minimum detoriation
+	OvmsMetricFloat*  m_b_bat_avail_energy;      // Battery available kWh
+	OvmsMetricFloat*  m_b_bat_max_charge_power;  // Max Bat Charge
+	OvmsMetricFloat*  m_mains_power;             // Mains Power Consumed
+	OvmsMetricFloat*  m_v_hydraulic_brake_power; // Hydraulic Brake Power Request (Losses)
+	OvmsMetricFloat*	m_bat_heat_sink_temp;
+	OvmsMetricInt* m_charge_pilot_current;
+	OvmsMetricInt* m_b_temp1;
+	OvmsMetricInt* m_b_temp2;
+	OvmsMetricInt* m_b_temp3;
+	OvmsMetricInt* m_b_temp4;
+	OvmsMetricInt* m_b_temp5;
+	OvmsMetricInt* m_b_temp6;
+	OvmsMetricInt* m_b_temp7;
+	OvmsMetricInt* m_b_temp8;
+	OvmsMetricInt* m_b_temp9;
+	OvmsMetricInt* m_b_temp10;
+	OvmsMetricInt* m_b_temp11;
+	OvmsMetricInt* m_b_temp12;
 
-//float rz_battery_max_detoriation;                 //02 21 05 -> 24 1+2
-//float rz_battery_min_detoriation;                 //02 21 05 -> 24 4+5
-
-
-#define CFG_DEFAULT_MAXRANGE 160
-int rz_maxrange = CFG_DEFAULT_MAXRANGE;        // Configured max range at 20 °C
-
-#define CGF_DEFAULT_BATTERY_CAPACITY 27000
-float rz_battery_capacity = CGF_DEFAULT_BATTERY_CAPACITY; //TODO Detect battery capacity from VIN or number of batterycells
-
-char m_vin[18];
-
-uint32_t rz_tpms_id[4];
-float rz_obc_volt;
-
-float rz_bat_current;
-float rz_bat_voltage;
-float rz_bat_power;
+	//float rz_battery_max_detoriation;                 //02 21 05 -> 24 1+2
+	//float rz_battery_min_detoriation;                 //02 21 05 -> 24 4+5
 
 
-int8_t rz_battery_module_temp[12];
+#define CFG_DEFAULT_MAXRANGE 140
+	int rz_maxrange = CFG_DEFAULT_MAXRANGE;        // Configured max range at 20 °C
 
-INT rz_battery_current;                                 //Battery current               02 21 01 -> 21 7+22 1
-INT rz_charge_state_local;
+#define CGF_DEFAULT_BATTERY_CAPACITY 26000
+	float rz_battery_capacity = CGF_DEFAULT_BATTERY_CAPACITY; //TODO Detect battery capacity from VIN or number of batterycells
 
-uint32_t rz_battery_cum_charge_current;         //Cumulated charge current    02 21 01 -> 24 6+7 & 25 1+2
-uint32_t rz_battery_cum_discharge_current;    //Cumulated discharge current 02 21 01 -> 25 3-6
-uint32_t rz_battery_cum_charge_power;                         //Cumulated charge power      02 21 01 -> 25 7 + 26 1-3
-uint32_t rz_battery_cum_discharge_power;                 //Cumulated discharge power   02 21 01 -> 26 4-7
-uint32_t rz_battery_cum_op_time;                     //Cumulated operating time    02 21 01 -> 27 1-4
+	char m_vin[18];
 
-uint8_t rz_battery_cell_voltage[100];
+	uint32_t rz_tpms_id[4];
+	float rz_obc_volt;
 
-uint8_t rz_battery_min_temperature;             //02 21 05 -> 21 7
-uint8_t rz_battery_inlet_temperature;         //02 21 05 -> 21 6
-uint8_t rz_battery_max_temperature;             //02 21 05 -> 22 1
-uint8_t rz_battery_heat_1_temperature;     //02 21 05 -> 23 6
-uint8_t rz_battery_heat_2_temperature;     //02 21 05 -> 23 7
+	float rz_bat_current;
+	float rz_bat_voltage;
+	float rz_bat_power;
 
-uint8_t rz_charge_pilot_current;
-float rz_heatsink_temperature; //TODO Remove?
 
-struct {
-    unsigned char ChargingChademo : 1;
-    unsigned char Charging : 1;
-    unsigned char : 1;
-    unsigned char : 1;
-    unsigned char FanStatus : 4;
-} rz_charge_bits;
+	int8_t rz_battery_module_temp[12];
 
-struct {
-    uint8_t byte[8];
-    uint8_t status;
-    uint16_t id;
-}  rz_send_can;
+	INT rz_battery_current;												//Battery current 	02 21 01 -> 21 7+22 1
+	INT rz_charge_state_local;
 
-const TickType_t xDelay = 50 / portTICK_PERIOD_MS;
+	uint32_t rz_battery_cum_charge_current;				//Cumulated charge current    02 21 01 -> 24 6+7 & 25 1+2
+	uint32_t rz_battery_cum_discharge_current;		//Cumulated discharge current 02 21 01 -> 25 3-6
+	uint32_t rz_battery_cum_charge_power;					//Cumulated charge power      02 21 01 -> 25 7 + 26 1-3
+	uint32_t rz_battery_cum_discharge_power;      //Cumulated discharge power   02 21 01 -> 26 4-7
+	uint32_t rz_battery_cum_op_time;              //Cumulated operating time    02 21 01 -> 27 1-4
+
+	uint8_t rz_battery_cell_voltage[100];
+
+	uint8_t rz_battery_min_temperature;					//02 21 05 -> 21 7
+	uint8_t rz_battery_inlet_temperature;				//02 21 05 -> 21 6
+	uint8_t rz_battery_max_temperature;					//02 21 05 -> 22 1
+	uint8_t rz_battery_heat_1_temperature;			//02 21 05 -> 23 6
+	uint8_t rz_battery_heat_2_temperature;			//02 21 05 -> 23 7
+
+	uint8_t rz_charge_pilot_current;
+	float rz_heatsink_temperature;							//TODO Remove?
+
+	struct {
+			unsigned char ChargingChademo : 1;
+			unsigned char Charging : 1;
+			unsigned char : 1;
+			unsigned char : 1;
+			unsigned char FanStatus : 4;
+	} rz_charge_bits;
+
+	struct {
+			uint8_t byte[8];
+			uint8_t status;
+			uint16_t id;
+	}  rz_send_can;
+
+	const TickType_t xDelay = 50 / portTICK_PERIOD_MS;
 };
 
 #define SQR(n) ((n)*(n))
@@ -207,6 +207,5 @@ const TickType_t xDelay = 50 / portTICK_PERIOD_MS;
 
 #define SMART_JUNCTION_BOX 0x771
 #define BODY_CONTROL_MODULE  0x7A0
-
 
 #endif //#ifndef __VEHICLE_RENAULTZOE_H__
